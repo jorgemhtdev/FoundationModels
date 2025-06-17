b98cc3a2c1af78d0e8d351b3482077d12e4e2392
jorge <jorgemrht@icloud.com>
2025-06-17 23:59:37 +0200

    hello world

---
+++ books/books/booksApp.swift
@@ -0,0 +1,17 @@
+//
+//  booksApp.swift
+//  books
+//
+//  Created by jorge on 17/6/25.
+//
+
+import SwiftUI
+
+@main
+struct booksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
